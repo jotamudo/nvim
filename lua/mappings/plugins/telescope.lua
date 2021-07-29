21666d298f377ba6c1a036d47ae4d2d21bb9d357
jotamudo <juanvasconceloslopes@gmail.com>
2021-07-29 09:42:25 -0300

    Added search in symbols through telescope

--- lua/mappings/plugins/telescope.lua
+++ lua/mappings/plugins/telescope.lua
@@ -5,6 +5,7 @@ local mappings = {
   ["n<leader>fg"] = {"<cmd>lua require('telescope.builtin').live_grep()<cr>", noremap = true},
   ["n<leader>fb"] = {"<cmd>lua require('telescope.builtin').buffers()<cr>", noremap = true},
   ["n<leader>fh"] = {"<cmd>lua require('telescope.builtin').help_tags()<cr>", noremap = true},
+  ["n<leader>fs"] = {"<cmd>lua require('telescope.builtin').lsp_workspace_symbols()<cr>", noremap = true},
 }
 nvim_apply_mappings(mappings)
 
