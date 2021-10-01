d082c92f3f7818e49fb2f36ac5e15bc8d570b970
jotamudo <juanvasconceloslopes@gmail.com>
2021-10-01 00:12:48 -0300

    fixed vimtex issue

--- lua/plugins/latex/vimtex.lua
+++ lua/plugins/latex/vimtex.lua
@@ -2,5 +2,5 @@ vim.cmd([[
   autocmd FileType tex let g:indentLine_enabled = 0
   let g:tex_flavor = 'latex'
   let g:vimtex_compiler_progname = 'nvr'
-  let g:vimtex_compiler_latexmk = { 'executable' : 'latexmk', 'options' : [ '-xelatex', '-file-line-error', '-synctex=1', '-interaction=nonstopmode', ], }
+  let g:vimtex_compiler_latexmk = { 'executable' : 'latexmk', 'options' : [ '-file-line-error', '-synctex=1', '-interaction=nonstopmode', '--shell-escape' ], }
 ]])
