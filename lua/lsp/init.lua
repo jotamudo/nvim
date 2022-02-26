e3141301007e9c72bf9f6d8239a300bba926d476
jotamudo <juanvasconceloslopes@gmail.com>
2022-02-26 20:59:53 -0300

    added clangd_extensions and grammar_guard

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -48,28 +48,105 @@ local saga = require 'lspsaga'
 -- server_filetype_map = {}
 saga.init_lsp_saga()
 
-lspconfig.clangd.setup{
-  on_attach=custom_attach,
-  --capabilities = {
-    --textDocument = {
-      --completion = {
-        --completionItem = {
-          --snippetSupport = true
-        --}
-      --}
-    --}
-  --},
-  --capabilities = capabilities,
-  init_options = {
-    usePlaceholders = true,
-    completeUnimported = true
-  },
-  flags = {
-    debounce_text_changes = 150,
+require("clangd_extensions").setup {
+    server = {
+        -- options to pass to nvim-lspconfig
+        -- i.e. the arguments to require("lspconfig").clangd.setup({})
+        on_attach=custom_attach,
+        capabilities = capabilities,
+        init_options = {
+          usePlaceholders = true,
+          completeUnimported = true
+        },
+        flags = {
+          debounce_text_changes = 150,
+        }
+    },
+    extensions = {
+        -- defaults:
+        -- Automatically set inlay hints (type hints)
+        autoSetHints = true,
+        -- Whether to show hover actions inside the hover window
+        -- This overrides the default hover handler
+        hover_with_actions = true,
+        -- These apply to the default ClangdSetInlayHints command
+        inlay_hints = {
+            -- Only show inlay hints for the current line
+            only_current_line = false,
+            -- Event which triggers a refersh of the inlay hints.
+            -- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
+            -- not that this may cause  higher CPU usage.
+            -- This option is only respected when only_current_line and
+            -- autoSetHints both are true.
+            only_current_line_autocmd = "CursorHold",
+            -- whether to show parameter hints with the inlay hints or not
+            show_parameter_hints = true,
+            -- whether to show variable name before type hints with the inlay hints or not
+            show_variable_name = false,
+            -- prefix for parameter hints
+            parameter_hints_prefix = "<- ",
+            -- prefix for all the other hints (type, chaining)
+            other_hints_prefix = "=> ",
+            -- whether to align to the length of the longest line in the file
+            max_len_align = false,
+            -- padding from the left if max_len_align is true
+            max_len_align_padding = 1,
+            -- whether to align to the extreme right or not
+            right_align = false,
+            -- padding from the right if right_align is true
+            right_align_padding = 7,
+            -- The color of the hints
+            highlight = "Comment",
+        },
+        ast = {
+            role_icons = {
+                type = "",
+                declaration = "",
+                expression = "",
+                specifier = "",
+                statement = "",
+                ["template argument"] = "",
+            },
+
+            kind_icons = {
+                Compound = "",
+                Recovery = "",
+                TranslationUnit = "",
+                PackExpansion = "",
+                TemplateTypeParm = "",
+                TemplateTemplateParm = "",
+                TemplateParamObject = "",
+            },
+
+            highlights = {
+                detail = "Comment",
+            },
+      }
   }
 }
 
-lspconfig.cmake.setup{ 
+lspconfig.grammar_guard.setup({
+  cmd = { env_vars.HOME .. '/.local/share/nvim/lsp_servers/ltex/ltex-ls/bin/ltex-ls' }, -- add this if you install ltex-ls yourself
+  settings = {
+    ltex = {
+      enabled = { "latex", "tex", "bib", "markdown", "pandoc", "vimwiki" },
+      language = "en",
+      diagnosticSeverity = "information",
+      setenceCacheSize = 2000,
+      additionalRules = {
+        enablePickyRules = true,
+        motherTongue = "en",
+      },
+      trace = { server = "verbose" },
+      dictionary = {},
+      disabledRules = {},
+      hiddenFalsePositives = {},
+      completionEnabled = true,
+    },
+  },
+})
+
+lspconfig.cmake.setup{
   on_attach=custom_attach,
   flags = {
     debounce_text_changes = 150,
@@ -215,17 +292,17 @@ lspconfig.cssls.setup{
   }
 }
 
-lspconfig.rust_analyzer.setup {
-  capabilities = capabilities,
-  completion = {
-    autoimport = {
-      enable = true
-    },
-  },
-  flags = {
-    debounce_text_changes = 150,
-  }
-}
+-- lspconfig.rust_analyzer.setup {
+--   capabilities = capabilities,
+--   completion = {
+--     autoimport = {
+--       enable = true
+--     },
+--   },
+--   flags = {
+--     debounce_text_changes = 150,
+--   }
+-- }
 
 -- lspconfig.svls.setup{
 --   on_attach = custom_attach
