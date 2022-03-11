4ac1d3b07ee2a2bdeeefa76cc07067554b63a8c7
jotamudo <juanvasconceloslopes@gmail.com>
2022-03-11 09:54:59 -0300

    changed luasnip structure

--- lua/plugins/luasnip/init.lua
+++ lua/plugins/luasnip/init.lua
@@ -2,9 +2,14 @@ require("luasnip.loaders.from_vscode").load()
 require("luasnip.loaders.from_snipmate").load()
 
 local types = require("luasnip.util.types")
+local ls = require('luasnip')
 
-require'luasnip'.config.setup({
+ls.config.setup({
+  -- Remember last snippet and let me go back to it even if outside selection
   history = true,
+
+  -- updating as I type
+  updateevents = "TextChanged, TextChangedI",
   ext_opts = {
     [types.choiceNode] = {
       active = {
