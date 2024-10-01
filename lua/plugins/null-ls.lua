89ac81a7a939dbeffd274f2c856a51c50d946029
j_mudo <jotamudo@riseup.net>
2024-10-01 13:31:21 -0300

    [fix] update linters on null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,10 +1,11 @@
 return {
   {
     "nvimtools/none-ls.nvim",
-    event = "BufReadPre",
+    event = {"BufReadPre", "BufNewFile"},
     dependencies = {
       "williamboman/mason.nvim",
-      "jayp0521/mason-null-ls.nvim"
+      "jayp0521/mason-null-ls.nvim",
+      "nvimtools/none-ls-extras.nvim",
     },
     config = true
   },
@@ -15,11 +16,21 @@ return {
     },
     config = function()
       local mason_null_ls = require("mason-null-ls")
+      local null_ls = require("null-ls")
       mason_null_ls.setup({
           automatic_installation = false,
           automatic_setup = true -- Recommended, but optional
         })
-      require("null-ls").setup()
+      null_ls.setup({
+        sources = {
+            null_ls.builtins.formatting.stylua,
+            null_ls.builtins.completion.spell,
+            null_ls.builtins.formatting.clang_format,
+            null_ls.builtins.formatting.yamlfix,
+            null_ls.builtins.formatting.yamlfmt,
+            require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
+        }
+      })
     end
   }
 }
