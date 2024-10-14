a27828a0accec87cdd1e973ca78b48ba0ee0d54e
j_mudo <jotamudo@riseup.net>
2024-10-13 23:32:15 -0300

    [update] remove unused plugins

--- lua/mappings/plugins/nvim-tree.lua
+++
@@ -1,3 +0,0 @@
-return {
-  -- {"<leader>e", ":NvimTreeToggle<CR>", {silent = true, desc = "[E]xplore"}}
-}
--- lua/plugins/nvim-tree.lua
+++
@@ -1,7 +0,0 @@
--- nothing
-return {
-  "kyazdani42/nvim-tree.lua",
-  dependencies = {"nvim-tree/nvim-web-devicons"},
-  keys = require("mappings.plugins.nvim-tree"),
-  config = true
-}
