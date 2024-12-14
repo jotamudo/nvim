3eecdce9c1c823876af027e6dc942fdc1a4330e5
j_mudo <jotamudo@riseup.net>
2024-12-13 23:42:09 -0300

    feat: add new dap mappings

--- lua/plugins/dap/mappings.lua
+++ lua/plugins/dap/mappings.lua
@@ -8,6 +8,9 @@ return {
   { '<leader>dlp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end },
   { '<leader>dr', function() require('dap').repl.open() end, },
   { '<leader>dl', function() require('dap').run_last() end },
+  { '<leader>dsn', function() require('dap').step_over() end },
+  { '<leader>dso', function() require('dap').step_out() end },
+  { '<leader>dsi', function() require('dap').step_into() end },
   { '<leader>dt', function() require('dapui').toggle() end },
   { '<leader>dps', function() require('dap-python').debug_selection() end, mode = "v" }
 }
