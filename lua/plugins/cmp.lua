935521365c56db54eaf5b3cd038b1d9a7373c233
Juan Vasconcelos Lopes <jotamudo@Juans-MacBook-Air.local>
2024-12-25 15:13:55 -0300

    wip: deprecate cmp.nvim

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,168 +1,170 @@
-return {
-  "iguanacucumber/magazine.nvim",
-  event = {"InsertEnter", "CmdlineEnter"},
-  keys = {":", "/"},
-  lazy = false,
-  -- enabled = false,
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-emoji",
-    "hrsh7th/cmp-nvim-lua",
-    "hrsh7th/cmp-calc",
-    "f3fora/cmp-spell",
-    "dmitmel/cmp-digraphs",
-    "uga-rosa/cmp-dictionary",
-    "hrsh7th/cmp-nvim-lsp-document-symbol",
-    "quangnguyen30192/cmp-nvim-tags",
-    "delphinus/cmp-ctags",
-    "ray-x/cmp-treesitter",
-    "hrsh7th/cmp-nvim-lsp-signature-help",
-    "onsails/lspkind-nvim",
-    "saadparwaiz1/cmp_luasnip",
-    "jmbuhr/otter.nvim"
-  },
-  config = function ()
-    vim.o.completeopt = 'menu,menuone,noselect'
-    local cmp = require('cmp')
-    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-    local lspkind = require('lspkind')
-    local types = require("cmp.types")
-    local str = require("cmp.utils.str")
-
-    local has_words_before = function()
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-
-    local luasnip = require("luasnip")
-
-    cmp.setup({
-      performance = {
-        debounce = 0
-      },
-      snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-          luasnip.lsp_expand(args.body) -- For `luasnip` users.
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          -- elseif luasnip.expand_or_jumpable() then
-          --   luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          -- elseif luasnip.jumpable(-1) then
-          --   luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      }),
-      sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'nvim-lua' },
-        { name = 'luasnip' }, -- For luasnip users.
-        { name = 'buffer' },
-        { name = 'path' },
-        { name = 'treesitter' },
-        { name = 'otter' },
-        { name = 'emoji' },
-        { name = 'spell' },
-        { name = 'calc' },
-        { name = 'dictionary' },
-        { name = 'ctags' },
-        { name = 'tags' },
-        -- { name = 'digraphs'},
-        -- { name = 'nuspell' },
-      }),
-      window = {
-        completion    = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      formatting = {
-        format = lspkind.cmp_format({
-          mode = 'symbol_text', -- show only symbol annotations
-          maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-
-          -- The function below will be called before any actual modifications from lspkind
-          -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-          before = function (entry, vim_item)
-            -- Get the full snippet (and only keep first line)
-            local word = entry:get_insert_text()
-            if entry.completion_item.insertTextFormat == types.lsp.InsertTextFormat.Snippet then
-                    word = word
-            end
-            word = str.oneline(word)
-
-            -- concatenates the string
-            -- local max = 50
-            -- if string.len(word) >= max then
-            --      local before = string.sub(word, 1, math.floor((max - 3) / 2))
-            --      word = before .. "..."
-            -- end
-
-            if
-              entry.completion_item.insertTextFormat == types.lsp.InsertTextFormat.Snippet
-              and string.sub(vim_item.abbr, -1, -1) == "~"
-            then
-              word = word .. "~"
-            end
-            vim_item.abbr = word
-
-            return vim_item
-          end
-        })
-      }
-
-    })
-
-    -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline('/', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = 'nvim_lsp_document_symbol' },
-        { name = 'buffer' }
-      }
-    })
-
-    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = 'path' }
-      }, {
-        { name = 'cmdline' }
-      })
-    })
-
-    -- dap REPL completion
-    require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
-      sources = {
-        { name = "dap" },
-      },
-    })
-
-    -- autopairs setup
-    cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
-  end
-}
+return {}
+-- return {
+--   "iguanacucumber/magazine.nvim",
+--   event = {"InsertEnter", "CmdlineEnter"},
+--   keys = {":", "/"},
+--   lazy = false,
+--   enabled = false,
+--   dependencies = {
+--     "hrsh7th/cmp-nvim-lsp",
+--     "hrsh7th/cmp-buffer",
+--     "hrsh7th/cmp-path",
+--     "hrsh7th/cmp-cmdline",
+--     "hrsh7th/cmp-emoji",
+--     "hrsh7th/cmp-nvim-lua",
+--     "hrsh7th/cmp-calc",
+--     "f3fora/cmp-spell",
+--     "rcarriga/cmp-dap",
+--     "dmitmel/cmp-digraphs",
+--     "uga-rosa/cmp-dictionary",
+--     "hrsh7th/cmp-nvim-lsp-document-symbol",
+--     "quangnguyen30192/cmp-nvim-tags",
+--     "delphinus/cmp-ctags",
+--     "ray-x/cmp-treesitter",
+--     "hrsh7th/cmp-nvim-lsp-signature-help",
+--     "onsails/lspkind-nvim",
+--     "saadparwaiz1/cmp_luasnip",
+--     "jmbuhr/otter.nvim",
+--   },
+--   config = function ()
+--     vim.o.completeopt = 'menu,menuone,noselect'
+--     local cmp = require('cmp')
+--     local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+--     local lspkind = require('lspkind')
+--     local types = require("cmp.types")
+--     local str = require("cmp.utils.str")
+--
+--     local has_words_before = function()
+--       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+--       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+--     end
+--
+--     local luasnip = require("luasnip")
+--
+--     cmp.setup({
+--       performance = {
+--         debounce = 0
+--       },
+--       snippet = {
+--         -- REQUIRED - you must specify a snippet engine
+--         expand = function(args)
+--           luasnip.lsp_expand(args.body) -- For `luasnip` users.
+--         end,
+--       },
+--       mapping = cmp.mapping.preset.insert({
+--         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+--         ['<C-f>'] = cmp.mapping.scroll_docs(4),
+--         ['<C-Space>'] = cmp.mapping.complete(),
+--         ['<C-e>'] = cmp.mapping.abort(),
+--         ['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+--
+--         ["<Tab>"] = cmp.mapping(function(fallback)
+--           if cmp.visible() then
+--             cmp.select_next_item()
+--           -- elseif luasnip.expand_or_jumpable() then
+--           --   luasnip.expand_or_jump()
+--           elseif has_words_before() then
+--             cmp.complete()
+--           else
+--             fallback()
+--           end
+--         end, { "i", "s" }),
+--
+--         ["<S-Tab>"] = cmp.mapping(function(fallback)
+--           if cmp.visible() then
+--             cmp.select_prev_item()
+--           -- elseif luasnip.jumpable(-1) then
+--           --   luasnip.jump(-1)
+--           else
+--             fallback()
+--           end
+--         end, { "i", "s" }),
+--       }),
+--       sources = cmp.config.sources({
+--         { name = 'nvim_lsp' },
+--         { name = 'nvim-lua' },
+--         { name = 'luasnip' }, -- For luasnip users.
+--         { name = 'buffer' },
+--         { name = 'path' },
+--         { name = 'treesitter' },
+--         { name = 'otter' },
+--         { name = 'emoji' },
+--         { name = 'spell' },
+--         { name = 'calc' },
+--         { name = 'dictionary' },
+--         { name = 'ctags' },
+--         { name = 'tags' },
+--         -- { name = 'digraphs'},
+--         -- { name = 'nuspell' },
+--       }),
+--       window = {
+--         completion    = cmp.config.window.bordered(),
+--         documentation = cmp.config.window.bordered(),
+--       },
+--       formatting = {
+--         format = lspkind.cmp_format({
+--           mode = 'symbol_text', -- show only symbol annotations
+--           maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+--
+--           -- The function below will be called before any actual modifications from lspkind
+--           -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+--           before = function (entry, vim_item)
+--             -- Get the full snippet (and only keep first line)
+--             local word = entry:get_insert_text()
+--             if entry.completion_item.insertTextFormat == types.lsp.InsertTextFormat.Snippet then
+--                     word = word
+--             end
+--             word = str.oneline(word)
+--
+--             -- concatenates the string
+--             -- local max = 50
+--             -- if string.len(word) >= max then
+--             --      local before = string.sub(word, 1, math.floor((max - 3) / 2))
+--             --      word = before .. "..."
+--             -- end
+--
+--             if
+--               entry.completion_item.insertTextFormat == types.lsp.InsertTextFormat.Snippet
+--               and string.sub(vim_item.abbr, -1, -1) == "~"
+--             then
+--               word = word .. "~"
+--             end
+--             vim_item.abbr = word
+--
+--             return vim_item
+--           end
+--         })
+--       }
+--
+--     })
+--
+--     -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+--     cmp.setup.cmdline('/', {
+--       mapping = cmp.mapping.preset.cmdline(),
+--       sources = {
+--         { name = 'nvim_lsp_document_symbol' },
+--         { name = 'buffer' }
+--       }
+--     })
+--
+--     -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+--     cmp.setup.cmdline(':', {
+--       mapping = cmp.mapping.preset.cmdline(),
+--       sources = cmp.config.sources({
+--         { name = 'path' }
+--       }, {
+--         { name = 'cmdline' }
+--       })
+--     })
+--
+--     -- dap REPL completion
+--     require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+--       sources = {
+--         { name = "dap" },
+--       },
+--     })
+--
+--     -- autopairs setup
+--     cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+--   end
+-- }
