76c578cd0ad382e1c27d22ce457380e9a7bea897
jotamudo <jotamudo@riseup.net>
2025-02-09 19:09:58 -0300

    feat: add ruff support to formatting and linting

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -27,25 +27,47 @@ return {
                     'yamlfix',
                     'yamlfmt',
                     'eslint',
+                    'ruff',
                 },
                 automatic_installation = false,
                 automatic_setup = true, -- Recommended, but optional
                 handlers = {
+                    function(source_name, methods)
+                        -- custom logic
+                        require('mason-null-ls').default_setup(
+                            source_name,
+                            methods
+                        ) -- to maintain default behavior
+                    end,
                     -- function() end, -- disables automatic setup of all null-ls sources
-                    -- stylua = function(source_name, methods)
-                    -- 	null_ls.register(null_ls.builtins.formatting.stylua)
-                    -- end,
-                    -- shfmt = function(source_name, methods)
-                    -- 	-- custom logic
-                    -- 	require("mason-null-ls").default_setup(source_name, methods) -- to maintain default behavior
-                    -- end,
+                    stylua = function(source_name, methods)
+                        null_ls.register(null_ls.builtins.formatting.stylua)
+                    end,
+                    shfmt = function(source_name, methods)
+                        -- custom logic
+                        require('mason-null-ls').default_setup(
+                            source_name,
+                            methods
+                        ) -- to maintain default behavior
+                    end,
+                    ruff = function(source_name, methods)
+                        -- custom logic
+                        require('mason-null-ls').default_setup(
+                            source_name,
+                            methods
+                        ) -- to maintain default behavior
+                    end,
                 },
             })
-            local null_ls = require('null-ls')
             null_ls.setup({
                 sources = {
                     null_ls.builtins.completion.spell,
+                    require('none-ls.formatting.ruff_format'), -- requires none-ls-extras.nvim
+                    require('none-ls.diagnostics.ruff'), -- requires none-ls-extras.nvim
+                    null_ls.builtins.formatting.black,
                     require('none-ls.diagnostics.eslint'), -- requires none-ls-extras.nvim
+                    require('none-ls.diagnostics.cpplint'), -- requires none-ls-extras.nvim
+                    require('none-ls.code_actions.eslint'), -- requires none-ls-extras.nvim
                 },
             })
         end,
