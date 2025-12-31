2e5bfcfcc94518f0baa7681be9ae1c9e39135787
jotamudo <jotamudo@riseup.net>
2025-12-31 17:05:23 +0100

    fix: disable broken language parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -19,6 +19,7 @@ return {
     config = function()
         require('nvim-treesitter.configs').setup({
             ensure_installed = 'all', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+            ignore_install = {"ipkg"},
             highlight = {
                 enable = true, -- false will disable the whole extension
                 --additional_vim_regex_highlighting = { "markdown" }
