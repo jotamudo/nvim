1a585b639f269daf80d9a584d3f3186a48d0d660
jotamudo <jotamudo@riseup.net>
2026-06-16 03:51:44 +0200

    fix: indenting

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,20 +3,20 @@ return {
     ft = { 'tex' },
     config = function()
         vim.cmd([[
-  autocmd FileType tex let g:indentLine_enabled = 0
-  let g:vimtex_compiler_latexmk = { 
-    \ 'out_dir': 'out', 
-    \ 'aux_dir': 'out', 
-    \ 'executable' : 'latexmk', 
-    \ 'options' : [ 
-    \ '-file-line-error', 
-    \ '-synctex=1', 
-    \ '-interaction=nonstopmode', 
-    \ '--shell-escape' 
-    \ ], 
-    \ }
-  let g:vimtex_view_general_viewer = 'skim'
-  let g:vimtex_view_method = 'skim'
-]])
+          autocmd FileType tex let g:indentLine_enabled = 0
+          let g:vimtex_compiler_latexmk = { 
+            \ 'out_dir': 'out', 
+            \ 'aux_dir': 'out', 
+            \ 'executable' : 'latexmk', 
+            \ 'options' : [ 
+            \ '-file-line-error', 
+            \ '-synctex=1', 
+            \ '-interaction=nonstopmode', 
+            \ '--shell-escape' 
+            \ ], 
+            \ }
+          let g:vimtex_view_general_viewer = 'skim'
+          let g:vimtex_view_method = 'skim'
+        ]])
     end,
 }
