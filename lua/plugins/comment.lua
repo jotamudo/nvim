544f568dd46f5cef2e89a1a457667f96176b0bd4
jotamudo <jotamudo@riseup.net>
2026-06-16 03:52:03 +0200

    fix: add comments plugins

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,4 +1,12 @@
-return {
-    'numToStr/Comment.nvim',
-    config = true,
+return 
+-- {
+--     'numToStr/Comment.nvim',
+--     config = true,
+-- }
+
+{
+  "folke/ts-comments.nvim",
+  opts = {},
+  event = "VeryLazy",
+  enabled = vim.fn.has("nvim-0.10.0") == 1,
 }
